e8d01b3d750a527e6f13eab2a7ff1cd0f4ecde33
Stousn <stefan.reip@edu.fh-joanneum.at>
2021-05-07 18:14:55 +0200

    fix header on mobile screens

--- public/views/components/Navbar.js
+++ public/views/components/Navbar.js
@@ -12,6 +12,9 @@ let Navbar = {
                 <a class="w3-bar-item w3-button" href="/#/privacy">Datenschutz</a>
                 <homepage></homepage>
             </div>
+            <nav class="w3-bar w3-large w3-theme-d3 w3-hide-medium w3-hide-large" role="title" aria-label="main title">
+                <span class="w3-bar-item" style="text-align: center; width: 100%;"><a href="/#/" style="text-decoration: none" id="sitename-small">stousn.dev</a></span>
+            </nav>
             <nav class="w3-bar w3-large w3-theme-d3" role="navigation" aria-label="main navigation">
                 <div class="container">
                     <div>
@@ -20,8 +23,8 @@ let Navbar = {
                         </a>
                     </div>
                 </div>
-                <span class="w3-bar-item"><a href="/#/" style="text-decoration: none" id="sitename">stousn.dev</a></span>
-                <span class="w3-bar-item">></span>
+                <span class="w3-bar-item w3-hide-small"><a href="/#/" style="text-decoration: none" id="sitename">stousn.dev</a></span>
+                <span class="w3-bar-item w3-hide w3-hide-small">></span>
                 <span id="pagename" class="w3-bar-item">Projects</span>
                 <a href="https://www.linkedin.com/company/stefanreip-dev" target="_blank"><span id="linkedin" class="w3-bar-item w3-right w3-button"><i class="fa fa-linkedin"></i></span></a>
                 <a href="https://www.facebook.com/stefanreip.dev/" target="_blank"><span id="facebook" class="w3-bar-item w3-right w3-button"><i class="fa fa-facebook"></i></span></a>
@@ -39,13 +42,19 @@ let Navbar = {
         });
         function onPageInit() {
             window.title = window.location.hostname.replace('www.', '')
-            document.getElementById('sitename').innerText = window.location.hostname.replace('www.', '')
+            editSiteName('sitename')
+            editSiteName('sitename-small')
+
             document.getElementsByTagName('homepage')[0].innerHTML = '<a href = "https://' +
                 window.location.hostname.replace('www.', '').replace('dev', 'at') +
                 '" class="w3-bar-item w3-button w3-theme-l4" >' +
                 window.location.hostname.replace('www.', '').replace('dev', 'at') +
                 '</a >'
         } onPageInit()
+
+        function editSiteName(name) {
+            document.getElementById(name).innerText = window.location.hostname.replace('www.', '')
+        }
     }
 
 }
